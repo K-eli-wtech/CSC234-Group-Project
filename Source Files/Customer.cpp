7af3396467fc60cb40cf464cc30246f748f382e0
K-eli-wtech <kewhite3@my.waketech.edu>
2022-04-27 15:59:55 -0400

    Updates to Source and Customer

--- Source Files/Customer.cpp
+++ Source Files/Customer.cpp
@@ -16,82 +16,66 @@
 using namespace std;
 
 
-ostream& operator<<(ostream& out, const Customer& customer)
-{
-	out << "***************************************************************************************************\n";
-	out << "Name:      " << customer.name << '\n';
-	out << "Address:   " << customer.address << '\n';
-	cout << "Email:    " << customer.email << '\n';
-	"------------ Book Orders -------------";
-	cout << customer.orders;
+ostream& operator<<(ostream& out, const Customer& customer) {
+    out << "***************************************************************************************************\n";
+    out << "Name:      " << customer.name << '\n';
+    out << "Address:   " << customer.address << '\n';
+    cout << "Email:    " << customer.email << '\n';
+    "------------ Book Orders -------------";
+    cout << customer.orders;
 }
 
-Customer::Customer()
-{
-	name = "";
-	address = "";
-	email = "";
-	orders = OrderList();
+Customer::Customer() {
+    name = "";
+    address = "";
+    email = "";
+    orders = OrderList();
 }
 
-Customer::Customer(string _name, string _address, string _email, OrderList _orders)
-{
-	name = _name;
-	address = _address;
-	email = _email;
-	orders = _orders;
+Customer::Customer(string _name, string _address, string _email, OrderList _orders) {
+    this->name = _name;
+    this->address = _address;
+    this->email = _email;
+    this->orders = _orders;
 
 }
 
-OrderList Customer::getOrders()
-{
-	return orders;
+OrderList Customer::getOrders() {
+    return this->orders;
 }
 
-void Customer::AddOrder(Order order)
-{
-	orders.AddOrder(order);
-}
-
-void Customer::UpdateOrders(string, int)
-{
+void Customer::AddOrder(Order) {
 
 }
 
-void Customer::CancelOrder(string)
-{
+void Customer::UpdateOrders(string, int) {
 
 }
 
-string Customer::getCustomerName()
-{
-	return name;
-}
-
-string Customer::getAddress()
-{
-	return address;
-}
-
-string Customer::getEmail()
-{
-	return email;
-}
-
-double Customer::checkoutOrders()
-{
+void Customer::CancelOrder(string) {
 
 }
 
-bool Customer::operator==(const Customer& comp) const
-{
-	/* Correct if this is wrong, but I believe this *
-	 * should return true if the names are equal    */
-	return name == comp.name;
+string Customer::getCustomerName() {
+    return this->name;
 }
 
-bool Customer::operator!=(const Customer& comp) const
-{
-	/* Same as == above, but != */
-	return name != comp.name;
+string Customer::getAddress() {
+    return this->address;
+}
+
+string Customer::getEmail() {
+    return this->email;
+}
+
+double Customer::checkoutOrders() {
+
+}
+
+bool Customer::operator==(const Customer& comp) const {
+    return this->name == comp.name;
+}
+
+bool Customer::operator!=(const Customer& comp) const {
+    return this->name != comp.name;
 }
--- Source Files/Source.cpp
+++ Source Files/Source.cpp
@@ -26,104 +26,143 @@ void PrintOrders(CustomerList&);
 void CheckoutOrders(CustomerList&);
 void UpdateDataFile(CustomerList&);
 
-int main()
-{
-	cout << "Welcome to Wake Bookstore!" << endl;
+int main() {
+    cout << "Welcome to Wake Bookstore!" << endl;
 
-	ifstream inFile;
-	inFile.open("BookOrders.txt");
+    ifstream inFile;
+    inFile.open("BookOrders.txt");
 
-	if(!inFile)
-	{
-		string subFile;
-		cout << "Failed to find the default file, please input file name." << endl;
-		cin >> subFile;
-		inFile.open(subFile);
-	}
+    if (!inFile) {
+        string subFile;
+        cout << "Failed to find the default file, please input file name." << endl;
+        cin >> subFile;
+        inFile.open(subFile);
+    }
 
-	int choice = selectMenu();
+    while (inFile) {
+        string name = "";
+        string address = "";
+        string email = "";
+        string book = "";
+        string cost = "";
+        string quantity = "";
+        string line;
 
-	switch (choice)
-	{
-	case 1:
-		PlaceOrder();
-		break;
-	case 2:
-		UpdateOrder();
-		break;
-	case 3:
-		CancelOrder();
-		break;
-	case 4:
-		PrintOrders();
-		break;
-	case 5:
-		CheckoutOrders();
-		break;
-	case 6:
-		cout << "Thank you for shopping at Wake Bookstore!" << endl;
-		break;
-	default:
-		cout << "Invalid choice" << endl;
-	}
+        getline(inFile, name);
+        getline(inFile, address);
+        getline(inFile, email);
+        Customer customer(name, address, email, orders);
 
-	return 0;
+        getline(inFile, book);
+        getline(inFile, cost);
+        getline(inFile, quantity);
+
+        // I don't think this part is going to work but I'm trying to loop it if the line doesn't start with % for the next name so we can store the orders
+        getline(inFile, line);
+        if (line.find("%") != string::npos) {
+            name = line;
+            cout << "its a name" << endl;
+            break;
+        }
+        else {
+            book = line;
+            getline(inFile, cost);
+            getline(inFile, quantity);
+            cout << book << "\n" << cost << "\n" << quantity << "\n" << endl;
+        }
+    }
+
+    int choice = selectMenu();
+
+    switch (choice)
+    {
+    case 1:
+        PlaceOrder(c_list);
+        break;
+    case 2:
+        UpdateOrder(c_list);
+        break;
+    case 3:
+        CancelOrder(c_list);
+        break;
+    case 4:
+        PrintOrders(c_list);
+        break;
+    case 5:
+        CheckoutOrders(c_list);
+        break;
+    case 6:
+        cout << "Thank you for shopping at Wake Bookstore!" << endl;
+        break;
+    default:
+        cout << "Invalid choice" << endl;
+    }
+
+    return 0;
 }
 
-void LoadCustomers(ifstream&, CustomerList&)
-{
-	if ()
-	{
-		cout << "All customers and orders are loaded." << endl;
-	}
-	else
-	{
-		cout << "Error: Issue loading customer data information." << endl;
-	}
-
-}
-
-int selectMenu()
-{
-	int select;
-	cout << "Please select one of the following actions: " << endl;
-	cout << "1: Place an order." << endl;
-	cout << "2: Update an order." << endl;
-	cout << "3: Cancel an order." << endl;
-	cout << "4: Print all orders." << endl;
-	cout << "5: Checkout Orders." << endl;
-	cout << "6: Exit \n" << endl;
-	cin >> select;
-	return select;
-
-}
-
-void PlaceOrder(CustomerList&)
-{
-
-}
-
-void UpdateOrder(CustomerList&)
-{
-
-}
-
-void CancelOrder(CustomerList&)
-{
-
-}
-
-void PrintOrders(CustomerList&)
-{
+void LoadCustomers(ifstream&, CustomerList&) {
+    if () {
+        cout << "All customers and orders are loaded." << endl;
+    }
+    else {
+        cout << "Error: Issue loading customer data information." << endl;
+    }
 
 }
 
-void CheckoutOrders(CustomerList&)
-{
+int selectMenu() {
+    int select;
+    cout << "Please select one of the following actions: " << endl;
+    cout << "1: Place an order." << endl;
+    cout << "2: Update an order." << endl;
+    cout << "3: Cancel an order." << endl;
+    cout << "4: Print all orders." << endl;
+    cout << "5: Checkout Orders." << endl;
+    cout << "6: Exit \n" << endl;
+    cin >> select;
+    return select;
 
 }
 
-void UpdateDataFile(CustomerList&)
-{
+void PlaceOrder(CustomerList&) {
+    cout << "Enter customer name: " << endl;
 
+    if () {
+
+    }
+    else {
+        cout << "Customer does not exist" << endl;
+    }
+}
+
+void UpdateOrder(CustomerList&) {
+    cout << "Enter customer name: " << endl;
+
+    if () {
+
+    }
+    else {
+        cout << "Customer does not exist" << endl;
+    }
+}
+
+void CancelOrder(CustomerList&) {
+    cout << "Enter customer name: " << endl;
+
+    if () {
+
+    }
+    else {
+        cout << "Customer does not exist" << endl;
+    }
+}
+
+void PrintOrders(CustomerList&) {
+}
+
+void CheckoutOrders(CustomerList&) {
+}
+
+void UpdateDataFile(CustomerList&) {
 }
